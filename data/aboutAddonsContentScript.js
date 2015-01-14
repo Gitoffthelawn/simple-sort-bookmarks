09f3e50a7eac570f9fbc6b9ef99212d7095493df
Nicola Felice <dev@dominiofelice.com>
2015-01-14 21:19:58 +0100

    Version 0.10.1
    - Add link to open the add-on manager from the advanced options tab
    - Replace depracated unsafeWindow

--- data/aboutAddonsContentScript.js
+++ data/aboutAddonsContentScript.js
@@ -9,10 +9,11 @@ var addonId = self.options.addonId;
 /*
 Solution found in:
 http://stackoverflow.com/questions/22593454/has-ff-addon-sdk-an-api-to-open-settings-page
+Replaced the deprecated "unsafeWindow" with "window" and it works.
 */
 self.port.on("activateAddonsSettings", function (data) {
     AddonManager.getAddonByID(addonId, function(aAddon) {
-        unsafeWindow.gViewController.commands.cmd_showItemDetails.doCommand(aAddon, true);
+        window.gViewController.commands.cmd_showItemDetails.doCommand(aAddon, true);
      });
 });
 
